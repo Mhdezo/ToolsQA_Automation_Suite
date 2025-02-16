994e33a905c05e621b16cdda044ab51194c4f1d1
Mohammad Alzoubi <72888565+Mhdezo@users.noreply.github.com>
2025-02-16 21:48:48 +0300

    Add files via upload

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
 # base class for all pages
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from logging_config import logging
+from ToolsQA_Automation_Suite.logging_config import logging
+from selenium.webdriver.common.alert import Alert
 
 class BasePage:
     def __init__(self, driver):
@@ -34,3 +35,33 @@ class BasePage:
         except Exception as e:
             logging.error(f"Failed of element visibility: {e}")
             return False
+
+    def is_alert_present(self):
+        """Check if alert is visible"""
+        try:
+            WebDriverWait(self.driver,10).until(EC.alert_is_present())
+            return True
+        except Exception as e:
+            logging.error(f"Failed of alert visibility: {e}")
+            return False
+
+    def accept_alert(self):
+        try:
+            Alert(self.driver).accept()
+            logging.info(f'Alert has been accepted')
+        except Exception as e:
+            logging.error(f"Failed to accept alert: {e}")
+
+    def cancel_alert(self):
+        try:
+            Alert(self.driver).dismiss()
+            logging.info(f'Alert has been dismissed')
+        except Exception as e:
+            logging.error(f"Failed to dismiss alert: {e}")
+
+    def type_alert(self, prompt):
+        try:
+            Alert(self.driver).send_keys(prompt)
+            logging.info(f'Prompt has been written with: {prompt}')
+        except Exception as e:
+            logging.error(f"Failed type in the prompt field: {e}")
